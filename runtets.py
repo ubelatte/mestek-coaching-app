67178fca4bb03f312b6cfa6cd1f2d0bb4a74551e
Luna Pham <wfhq_lpham@mestekcorp.com>
2025-07-10 13:07:57 -0400

    Updated streamlit_app.py and added runtets.py

--- runtets.py
+++
@@ -1,9 +0,0 @@
-from google.auth.transport.requests import Request
-from google.auth import exceptions
-
-# Revoke and reset credentials
-credentials = None
-
-# Assuming your service account is being loaded here
-if credentials and credentials.expired and credentials.refresh_token:
-    credentials.refresh(Request())
